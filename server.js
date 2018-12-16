27da7867ccc117c38ffc47071d4f1b28c797f2a3
dmegahan <dannymegahan@hotmail.com>
2018-12-15 17:02:44 -0700

    'Full' express implementation, css and images properly loading now

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require('express');
 
 var server = express();
 
+server.use(express.static(__dirname + '/src/images'));
+server.use(express.static(__dirname + '/src/components'));
+
 server.get('/', (request, response) => {
     routing.home(request, response);
 });
